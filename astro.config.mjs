10e50cf8231b4aeeecfc48b5761a10e85f33810f
SeryiBaran <seryibaranminepy@yandex.ru>
2023-02-23 17:51:28 +0300

    try to get auto cache updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,6 +37,7 @@ export default defineConfig({
       config: { applyBaseStyles: false },
     }),
     AstroPWA({
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: !isBuild,
       },
