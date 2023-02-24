9d3a277f90899904acf1a38848db725001368deb
SeryiBaran <seryibaranminepy@yandex.ru>
2023-02-24 21:11:55 +0300

    fix darkmode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ function withOpacity(variableName) {
 }
 
 module.exports = {
+  darkMode: ['class', '[data-theme="dark"]'],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
