b8a1e736f8967d8603cf4bfbc6fea9fe20778055
SeryiBaran <seryibaranminepy@yandex.ru>
2023-02-24 16:38:19 +0300

    fix
    https://github.com/evanw/esbuild/issues/1051#issuecomment-1006992549

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://seryibaran.github.io/",
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
   integrations: [
     tailwind({
       config: {
