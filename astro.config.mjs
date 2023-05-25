16b36c316104ad700f592608f20202e1e4fe3091
SeryiBaran <seryibaranminepy@yandex.ru>
2023-05-25 13:11:53 +0300

    astro-critters + astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,24 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import critters from "astro-critters";
 import { astroImageTools } from "astro-imagetools";
-
+import compress from "astro-compress";
 import remarkToc from "./remarkPlugins/remark-toc.patched";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://seryibaran.github.io",
-  integrations: [mdx(), sitemap(), astroImageTools],
+  integrations: [mdx(), sitemap(), astroImageTools, critters(), compress()],
   markdown: {
-    remarkPlugins: [[remarkToc, { heading: "Содержание" }]],
+    remarkPlugins: [
+      [
+        remarkToc,
+        {
+          heading: "Содержание",
+        },
+      ],
+    ],
     extendDefaultPlugins: true,
   },
 });
