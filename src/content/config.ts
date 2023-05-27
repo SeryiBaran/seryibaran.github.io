503b7ed69f790ea52df2ae8a8ce211c76f48fc49
SeryiBaran <seryibaranminepy@yandex.ru>
2023-05-27 22:05:01 +0300

    fragmenty-coda: improve generatePyramid

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
   // Type-check frontmatter using a schema
@@ -16,6 +16,6 @@ const blog = defineCollection({
       .transform((str) => (str ? new Date(str) : undefined)),
     heroImage: z.string().optional(),
   }),
-});
+})
 
-export const collections = { blog };
+export const collections = { blog }
