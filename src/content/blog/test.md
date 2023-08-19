91617e03de1aad2e8465ee454d0977e052959cd1
SeryiBaran <seryibaranminepy@yandex.ru>
2023-08-19 14:30:57 +0300

    add test post

---
+++ src/content/blog/test.md
@@ -0,0 +1,8 @@
+---
+title: Test
+description: ''
+date: 2023-08-19T11:30:35.205Z
+heroImage: ''
+---
+
+Test
--- telegram-sender-bot/index.ts
+++ telegram-sender-bot/index.ts
@@ -6,38 +6,39 @@ if (
   !process.env.TG_CHANNEL_ID ||
   !process.env.TG_BOT_APIKEY
 )
-  throw new Error(
+  console.error(
     'no created file or CREATED_FILE or TG_CHANNEL_ID or TG_BOT_APIKEY is not defined!',
   )
+else {
+  const CREATED_FILE = process.env.CREATED_FILE
+  const TG_CHANNEL_ID = process.env.TG_CHANNEL_ID
+  const TG_BOT_APIKEY = process.env.TG_BOT_APIKEY
 
-const CREATED_FILE = process.env.CREATED_FILE
-const TG_CHANNEL_ID = process.env.TG_CHANNEL_ID
-const TG_BOT_APIKEY = process.env.TG_BOT_APIKEY
+  const postSlugs = CREATED_FILE.split(' ')
+    .filter((file) => /.+\.md$/gi.test(file) || /.+\.mdx/gi.test(file))
+    .join('__DELIMETEER__1__')
+    .replaceAll(/^src\/content\/blog\//gi, '')
+    .replaceAll(/\.mdx$/gi, '')
+    .replaceAll(/\.md$/gi, '')
 
-const postSlugs = CREATED_FILE.split(' ')
-  .filter((file) => /.+\.md$/gi.test(file) || /.+\.mdx/gi.test(file))
-  .join('__DELIMETEER__1__')
-  .replaceAll(/^src\/content\/blog\//gi, '')
-  .replaceAll(/\.mdx$/gi, '')
-  .replaceAll(/\.md$/gi, '')
+  if (!postSlugs.length) {
+    console.log('No posts created!')
+    process.exit(0)
+  }
 
-if (!postSlugs.length) {
-  console.log('No posts created!')
-  process.exit(0)
-}
+  const bot = new Bot(TG_BOT_APIKEY)
 
-const bot = new Bot(TG_BOT_APIKEY)
-
-async function sendMessage(slug: string) {
-  const postUrl = `https://seryibaran.github.io/posts/${slug}`
-  await bot.api.sendMessage(
-    TG_CHANNEL_ID,
-    `🔔 На сайте вышел новый пост!
+  async function sendMessage(slug: string) {
+    const postUrl = `https://seryibaran.github.io/posts/${slug}`
+    await bot.api.sendMessage(
+      TG_CHANNEL_ID,
+      `🔔 На сайте вышел новый пост!
   <a href="postUrl">${postUrl}</a>`,
-    {
-      parse_mode: 'HTML',
-    },
-  )
-}
+      {
+        parse_mode: 'HTML',
+      },
+    )
+  }
 
-postSlugs.split('__DELIMETEER__1__').forEach(sendMessage)
+  postSlugs.split('__DELIMETEER__1__').forEach(sendMessage)
+}
