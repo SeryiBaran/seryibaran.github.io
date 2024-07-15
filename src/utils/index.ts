ad3e71d4632ecf28b8eeaa86193af8a1f0d6aa6e
SeryiBaran <seryibaranminepy@yandex.ru>
2024-07-15 11:10:23 +0300

    fix: date and time format

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,15 @@
+import type { Dayjs } from 'dayjs'
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 import { SITE_TITLE } from '@/consts'
 import 'dayjs/locale/ru'
+
+dayjs.extend(utc)
 
 export function getPageTitle(title: string) {
   return `${title} - ${SITE_TITLE}`
 }
 
-export function formatPostDate(date: Date, isShowTime: boolean = false) {
+export function formatPostDate(date: Date | Dayjs, isShowTime: boolean = false) {
   return dayjs(date.toISOString()).utc().locale('ru').format(isShowTime ? 'DD.MM.YYYY HH:mm UTC' : 'DD.MM.YYYY')
 }
