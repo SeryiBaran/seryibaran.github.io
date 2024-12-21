0f5136a5617d58b91997dd9bc9a568bb9ceba9be
SeryiBaran <seryibaranminepy@yandex.ru>
2024-12-21 22:42:21 +0300

    upd

---
+++ public/uploads/how_to_divide_cookie_by_3/DrawFunc.pas
@@ -0,0 +1,16 @@
+﻿uses GraphABC;
+
+function calcD3(x: real): real;
+begin
+  var a: real;
+  for var n:=1 to Floor(x) do
+  begin
+    a += ((power(-1,n+1)) / (power(2,n)));
+  end;
+  Result := a;
+end;
+
+begin
+  Brush.Color := ARGB(0,0,0,0);
+  Draw(x->calcD3(x), 1, 16);
+end.
